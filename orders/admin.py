dc44335d4d93a0ab8c92e5d6fe8e1ddb624e3e32
Timiemmy <faith.bolanle@yahoo.com>
2023-04-10 13:50:17 +0100

    add coupon app, override admin template in order and improve to export csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
+from django.urls import reverse
+import csv
+import datetime
+from django.http import HttpResponse
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    ''''
+    create an instance of HttpResponse, specifying the text/csv content type, to tell the 
+    browser that the response has to be treated as a CSV file.
+    '''
+    response = HttpResponse(content_type='text/csv')
+    # add a Content-Disposition header to indicate that the HTTP response contains an attached file
+    response['Content-Disposition'] = content_disposition
+    # ThisCSV writer object that will write to the response object.
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not
+              field.many_to_many and not field.one_to_many]
+    ''''
+    #get the model fields dynamically using the get_fields() method of the model’s _meta
+    options, and exclude many-to-many and one-to-many relationships.
+    '''
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:  # iterate over the given QuerySet
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+# customize the display name for the action in the actions drop-down element of the administration site
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
@@ -19,9 +59,25 @@ def order_payment(obj):
 order_payment.short_description = 'Stripe payment'
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
+''''
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+order_pdf.short_description = 'Invoice'
+'''
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated']
+                    'address', 'postal_code', 'city', 'paid', order_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -37,6 +37,7 @@ def order_create(request):
                   {'cart': cart, 'form': form})
 
 
+# decorator checks that both the is_active and is_staff fields of the user requesting the page are set to True.
 @staff_member_required
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
@@ -45,14 +46,21 @@ def admin_order_detail(request, order_id):
                   {'order': order})
 
 
+''''
 @staff_member_required
 def admin_order_pdf(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     html = render_to_string('orders/order/pdf.html',
-                            {'order': order})
+                            {'order': order})  # use the render_to_string() function provided by Django to render orders/order/pdf.html.
+    # generate a new HttpResponse object specifying the application/pdf content type
     response = HttpResponse(content_type='application/pdf')
+    # the Content-Disposition header to specify the filename.
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    # weasyprint.HTML(string=html).write_pdf(response,
-    # stylesheets=[weasyprint.CSS(
-    # settings.STATIC_ROOT / 'css/pdf.css')])
+    # use WeasyPrint to generate a PDF file from the rendered HTML code and write the file to the HttpResponse object
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT / 'css/pdf.css')])
+    # You use the static file css/pdf.css to add CSS styles to the generated PDF file. Then, you load it from
+    # the local path by using the STATIC_ROOT setting.
     return response
+'''
